818d1ee0f3d12ec6e860b137512770b68db8dd5b
emil14 <emil.musician@gmail.com>
2018-03-27 12:12:20 +0300

    Remove 'build' from repo

--- build/core/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./class/index");
-exports.Class = index_1.Class;
-exports.core = {
-    Class: index_1.Class,
-};
--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./core/index");
-exports.core = index_1.core;
