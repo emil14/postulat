28d1774b5e8cad1dae42d9dc09eb7884657c58b5
emil14 <emil.musician@gmail.com>
2019-04-03 21:21:37 +0300

    Add some test code

--- postulat/__init__.py
+++ postulat/__init__.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 from lark import Lark
 
-grammar_path = Path.cwd() / 'grammar.lark'
+cwd = Path.cwd()
+grammar_path = cwd / 'grammar.lark'
+syntax_test_path = cwd / 'syntax-test.txt'
+
 lark_parser = Lark(grammar_path.read_text())
+
+# print(lark_parser.parse(syntax_test_path.read_text()))
