4e6e6aa5a674d5a06b9d49e3d9087257d3955c76
emil14 <emil.musician@gmail.com>
2019-04-24 03:32:08 +0300

    Work in Progress

--- postulat/__init__.py
+++ postulat/__init__.py
@@ -7,4 +7,6 @@ syntax_test_path = cwd / 'syntax-test.txt'
 
 lark_parser = Lark(grammar_path.read_text())
 
-# print(lark_parser.parse(syntax_test_path.read_text()))
+print(
+    lark_parser.parse(syntax_test_path.read_text())
+)
