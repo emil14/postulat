7b37f8bdd5bdbaf6a293fb758ab6b43989bf54d8
emil14 <emil.musician@gmail.com>
2019-07-09 13:44:29 +0300

    Update grammar

--- postulat/__init__.py
+++ postulat/__init__.py
@@ -2,11 +2,11 @@ from pathlib import Path
 from lark import Lark
 
 cwd = Path.cwd()
-grammar_path = cwd / 'grammar.lark'
-syntax_test_path = cwd / 'syntax-test.txt'
 
-lark_parser = Lark(grammar_path.read_text(), propagate_positions=True)
+grammar_txt = (cwd / 'grammar.lark').read_text()
+syntax_test_txt = (cwd / 'syntax-test.txt').read_text()
 
-print(
-    lark_parser.parse(syntax_test_path.read_text()).pretty()
-)
+lark_parser = Lark(grammar_txt, propagate_positions=True)
+tree = lark_parser.parse(syntax_test_txt)
+
+print(tree.pretty())
